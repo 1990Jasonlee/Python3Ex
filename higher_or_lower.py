2754adcf54512560e9eb1310829994644629e7e5
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-14 13:36:10 -0400

    highlow

--- higher_or_lower.py
+++ higher_or_lower.py
@@ -1,2 +1,10 @@
 from random import randrange
 
+def high_low():
+
+    random_number = randrange(10)
+    guess = int(input("Your guess between 1-10 is: "))
+    print(f" The random number was: {random_number}.")
+
+    if random_number > guess:
+        print(f"Your guess was {guess}. Too high.")
