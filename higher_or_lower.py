9514aadd2d274a810502d2fbc4bd7784c0c0a011
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-14 13:39:36 -0400

    high low done

--- higher_or_lower.py
+++ higher_or_lower.py
@@ -1,10 +1,17 @@
 from random import randrange
 
-def high_low():
 
+def high_low():
     random_number = randrange(10)
     guess = int(input("Your guess between 1-10 is: "))
     print(f" The random number was: {random_number}.")
 
     if random_number > guess:
-        print(f"Your guess was {guess}. Too high.")
+        print(f"Your guess was {guess}.\n Too low.")
+    elif random_number < guess:
+        print(f"Your guess was {guess}.\n Too high.")
+    elif random_number == guess:
+        print(f"Your guess was {guess}.\n You guessed it correctly.")
+
+
+high_low()
