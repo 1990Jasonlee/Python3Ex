50c660a83144f2c95eb9d4a6d11f442dd0c96b06
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-14 15:52:40 -0400

    done

--- multilingual_greeter.py
+++ multilingual_greeter.py
@@ -1,12 +1,28 @@
 def language_input():
+    global pick
     pick = int(input('Enter number for corresponding language: 1-English 2-Spanish 3-French'))
 
-
-def greet(name):
-    print(f'Hello {name}')
-
 def name_input():
-    input_name = input('Enter a name:')
-    greet(input_name)
-name_input()
+    global input_name
+    if pick == 1:
+        input_name = input('Enter a name:')
+    elif pick == 2:
+        input_name = input('Ingresa un nombre:')
+    elif pick == 3:
+        input_name = input('Entrez un nom:')
+    else:
+        print('invalid choice')
 
+
+def greet():
+    if pick == 1:
+        print(f'Hello {input_name}')
+    elif pick == 2:
+        print(f'Hola {input_name}')
+    elif pick == 3:
+        print(f'Bonjour {input_name}')
+
+
+language_input()
+name_input()
+greet()
